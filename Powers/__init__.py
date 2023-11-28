23c36b50ac2b3b437cfbc7a0c9d0ece912c4ee4f
SiamRin33 <149977276+SiamRin33@users.noreply.github.com>
2023-11-29 00:04:13 +0600

    Update __init__.py

--- Powers/__init__.py
+++ Powers/__init__.py
@@ -27,7 +27,7 @@ file_handler = FileHandler(filename=LOGFILE)
 stdout_handler = StreamHandler(stdout)
 
 basicConfig(
-    format="%(asctime)s - [Gojo_Satoru] - %(levelname)s - %(message)s",
+    format="%(asctime)s - [Rin_Tohsaka] - %(levelname)s - %(message)s",
     level=INFO,
     handlers=[file_handler, stdout_handler],
 )
@@ -70,12 +70,12 @@ PYTHON_VERSION = python_version()
 PYROGRAM_VERSION = pyrogram.__version__
 
 LOGGER.info("------------------------")
-LOGGER.info("|      Gojo_Satoru      |")
+LOGGER.info("|      Rin_Tohsaka      |")
 LOGGER.info("------------------------")
 LOGGER.info(f"Version: {VERSION}")
 LOGGER.info(f"Owner: {str(Config.OWNER_ID)}")
 LOGGER.info(f"Time zone set to {Config.TIME_ZONE}")
-LOGGER.info("Source Code: https://github.com/Gojo-Bots/Gojo_Satoru\n")
+LOGGER.info("Source Code: https://github.com/SiamRin33/Group-Bot-V2\n")
 LOGGER.info("Checking lyrics genius api...")
 
 # API based clients
